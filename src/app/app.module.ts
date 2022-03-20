71bb09dc6ba3c53d00977912d98de470d518a60c
Dalfonzo <29Dalfonzo@gmail.com>
2022-03-20 02:35:24 -0400

    Components created and Header UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { TabsComponent } from './pages/tabs/tabs.component';
+import { SelectComponent } from './pages/select/select.component';
+import { ListComponent } from './pages/list/list.component';
+import { PagesComponent } from './pages/pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TabsComponent,
+    SelectComponent,
+    ListComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule
