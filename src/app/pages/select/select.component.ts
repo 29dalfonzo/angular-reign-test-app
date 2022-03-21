8ec841a51ff6671876a9008f0803140a7df16e1c
Dalfonzo <29Dalfonzo@gmail.com>
2022-03-20 23:46:56 -0400

    fix grid header / assets / select component done

--- src/app/pages/select/select.component.ts
+++ src/app/pages/select/select.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select.component.scss']
 })
 export class SelectComponent implements OnInit {
+	show: boolean = false;
+	options:Option[]=[
+		{name:'Angular', value:'angular', img:'assets/angularIcon.png'},
+		{name:'React', value:'react', img:'assets/reactIcon.png'},
+		{name:'Vue', value:'vue' , img:'assets/vueIcon.png'},
+	]
+
+	 selectedDropdown:Option={
+		name: 'Select your news',
+		value:'',
+		img: null
+	}
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectDropdown(option:Option){
+    this.selectedDropdown=option;
+    this.show=false;
+  }
+
+}
+
+interface Option{
+	name: string;
+	value: string;
+	img: string | null;
 }
