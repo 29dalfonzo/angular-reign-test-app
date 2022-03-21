d4b182e96a332fd4a5f18fd7881349a6c9138f4b
Dalfonzo <29Dalfonzo@gmail.com>
2022-03-21 01:22:28 -0400

    fix in grid layout and pages component with dinamic pages

--- src/app/components/pages/pages.component.ts
+++ src/app/components/pages/pages.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pages.component.scss']
 })
 export class PagesComponent implements OnInit {
+	pages=[1,2,3,4,5,6,7,8,9]
+	currentPage=1
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+	changePage(page:number){
+		//validate page if <1
+		if (page==0) return
+    if (page > this.pages[this.pages.length - 1]) {
+      // console.log('es mayor')
+      this.remakePages(page, true);
+      return;
+    }
+    if (page < this.pages[0]) {
+      // console.log('es menr')
+      this.remakePages(page, false);
+      return;
+    }
+		this.currentPage=page
+	}
+
+   remakePages(i: number, more: boolean) {
+		let newButton:number[] =[]
+    // take out the second element an add a new one (i+1) in button.length-2
+    if (more) {
+      newButton = this.pages.slice(1, this.pages.length);
+      newButton.push(i);
+    } else {
+      newButton = this.pages.slice(0, this.pages.length - 1);
+      newButton.unshift(i);
+    }
+			this.pages=newButton
+			this.currentPage=i
+  };
 
 }
